22d580da0b3d4dc6ac8631b2f29022d79ee0cf41
nidhi-baberwal <nidhibagoria29@gmail.com>
2026-08-09 21:57:21 +0530

    Add JWT authentication to login

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import prisma from "../config/prisma";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
+//register
 export const register = async (
     req: Request,
     res: Response
@@ -52,4 +54,68 @@ export const register = async (
 
     }
 
+};
+
+//login
+export const login = async(
+    req: Request,
+    res: Response
+) => {
+    try{
+
+    //get login data from frontend
+     const{ email, password } = req.body;
+
+     //find user by email
+     const user = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+     //user doesn't exist
+     if(!user) {
+        return res.status(401).json({
+            message: "Invalid email or password"
+        });
+     }
+
+     //compare entered password with hashed password
+     const isPasswordValid = await bcrypt.compare(
+        password,
+        user.password
+     );
+
+     //password is not valid
+     if (!isPasswordValid) {
+    return res.status(401).json({
+        message: "Invalid email or password"
+    });
+}
+    
+    //create token
+     const token = jwt.sign(
+        {userId: user.id},
+        process.env.JWT_SECRET as string,
+        {expiresIn: "7d"}
+     );
+
+     //send token to frontend
+
+     return res.status(200).json({
+        message: "Login successful",
+        token, 
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email
+        }
+     });
+     
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong"
+        });
+    }
 };
