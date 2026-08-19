6143a55f734a595c8b4ea212e1b6fa2718ab4934
nidhi-baberwal <nidhibagoria29@gmail.com>
2026-08-19 15:04:27 +0530

    feat: implement cart APIs

--- backend/src/controllers/cartController.ts
+++ backend/src/controllers/cartController.ts
@@ -41,6 +41,7 @@ export const createCart = async(
     }
 };
 
+//add to cart
 export const addToCart = async(
     req: Request,
     res: Response
@@ -107,4 +108,159 @@ export const addToCart = async(
             message: "Failed to add product to cart",
         });
     }
-}
+};
+
+//get current logged user's cart
+export const getCart = async(
+    req: Request,
+    res: Response
+) => {
+    try{
+        const userId = req.userId;
+
+        const cart = await prisma.cart.findUnique({
+            where: {
+                userId,
+            },
+            include: {
+                items: {
+                    include: {
+                        product: true,
+                    },
+                },
+            },
+        });
+
+        if(!cart){
+            return res.status(404).json({
+                message: "Cart not found",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Cart fetched successfully",
+            cart,
+        });
+        
+    } catch(error){
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Failed to get cart",
+        });
+    }
+};
+
+//update product quantity
+export const updateCartItem = async(
+    req: Request,
+    res: Response
+) => {
+    try{
+        const userId = req.userId;
+
+        const productId = Number(req.params.productId);
+
+        const { quantity } = req.body;
+
+        const cart = await prisma.cart.findUnique({
+            where: {
+                userId,
+            },
+        });
+
+        if(!cart){
+            return res.status(404).json({
+                message: "Cart not found",
+            });
+        }
+
+        const cartItem = await prisma.cartItem.findFirst({
+            where: {
+                cartId: cart.id,
+                productId: productId,
+            },
+        });
+
+        if(!cartItem){
+            return res.status(404).json({
+                message: "Product is not found in cart",
+            });
+        }
+
+        const updatedCartItem = await prisma.cartItem.update({
+            where: {
+                id: cartItem.id,
+            },
+            data: {
+                quantity,
+            },
+        });
+
+        return res.status(200).json({
+            message: "CartItem updated successfully",
+            cartItem: updatedCartItem,
+        });
+
+    } catch(error){
+        console.error(error)
+
+        return res.status(500).json({
+            message: "Something went wrong",
+        });
+    }
+};
+
+//delete product from cart
+export const deleteCartItem = async(
+    req: Request,
+    res: Response
+) => {
+    try{
+        const userId = req.userId;
+
+        const productId = Number(req.params.productId);
+
+        const cart = await prisma.cart.findUnique({
+            where: {
+                userId,
+            },
+        });
+
+        if(!cart){
+            return res.status(404).json({
+                message: "Cart not found",
+            });
+        }
+
+        const cartItem = await prisma.cartItem.findFirst({
+            where: {
+                cartId: cart.id,
+                productId: productId,
+            },
+        });
+
+        if(!cartItem){
+            return res.status(404).json({
+                message: "Product is not found in cart",
+            });
+        }
+
+       await prisma.cartItem.delete({
+        where: {
+            id:cartItem.id,
+        },
+       });
+
+        return res.status(200).json({
+            message: "product removed from cart successfully",
+        });
+
+    } catch(error){
+        console.error(error)
+
+        return res.status(500).json({
+            message: "Something went wrong",
+        });
+    }
+};
--- backend/src/routes/cartRoutes.ts
+++ backend/src/routes/cartRoutes.ts
@@ -1,10 +1,19 @@
 import express from "express";
-import { createCart , addToCart} from "../controllers/cartController";
+import { 
+    createCart, 
+    addToCart,
+    getCart,
+    updateCartItem,
+    deleteCartItem
+} from "../controllers/cartController";
 import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createCart);
 router.post("/items", authMiddleware, addToCart);
+router.get("/", authMiddleware, getCart);
+router.patch("/:productId", authMiddleware, updateCartItem);
+router.delete("/:productId", authMiddleware, deleteCartItem);
 
 export default router;
